7408213970876a55635bf511f61a01b0814c72bb
Karmen <karmenjot.minhas@gmail.com>
2024-03-29 23:40:58 -0400

    Created and styled layout for leaderboard page

---
+++ App/src/LeaderboardPage/index.js
@@ -0,0 +1,7 @@
+
+
+// Find the element with the class 'scrollable-element'
+var player_list = document.querySelector('.player_list');
+
+// Add a class to enable scrolling
+player_list.classList.add('scrollable');
